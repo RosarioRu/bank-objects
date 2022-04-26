915e744bf7f4e9210042f38b84359bbe0d2cbede
RosarioRu <mariarruval@gmail.com>
2022-04-25 19:32:35 -0700

    Add constructor to hold a list of all accts at a bank and accompanying prototype to addAccount to instances of AllAccountsAtBank.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,20 @@ function BankAccount(firstName, lastName, age, initialDeposit) {
 
 // prototype function that will be available to all instances of 'BankAccount'
 
+BankAccount.prototype.deposit = function () {
+  return this.initialDeposit + newDeposit;
+};
+
+// function to make/hold many instances of BankAccount objects.
+
+function AllAccountsAtBank() {
+  this.accounts = {};
+}
+
+AllAccountsAtBank.prototype.addAccount = function (account) {
+  this.accounts[account.firstName] = account;
+};
 
 
-String.prototype.addExcitement = function() { return this + "!!!!!!!!!" };
+
+
